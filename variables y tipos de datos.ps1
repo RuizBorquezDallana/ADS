58fb7d484d8bcf185cbf71caedae07277e06b219
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:43:44 -0700

    Create variables y tipos de datos.ps1

---
+++ variables y tipos de datos.ps1
@@ -0,0 +1,17 @@
+﻿$variable1 = "Hola "
+$variable2 = "Que tal?"
+$VARiable3 = 100
+${VAR iable4} = 200
+
+New-Variable -Name variable5 -Value 300
+$variable5
+
+$variable1 + $variable2
+$variable3 + ${VAR iable4}
+$variable3 - ${VAR iable4}
+
+$variable1 + $variable2
+
+$$
+$^
+$?
