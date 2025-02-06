2229993fb5dd297b9429ea1439b3a1c46d33a9fc
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:42:10 -0700

    Create Bucle Foreach.ps1

---
+++ Bucle Foreach.ps1
@@ -0,0 +1,19 @@
+﻿cat .\foreach1.ps1
+$soo = "freebsd", "openbsd", "solaris", "fedora", "ubuntu", "netbsd"
+foreach ($so in $soo)
+{
+    Write-Host $so
+}
+
+.\foreach1.ps1
+
+cat .\foreach2.ps1
+foreach ($archivo in Get-ChildItem)
+{
+    if ($archivo.length -ge 10KB)
+    {
+        Write-Host $archivo -> [($archivo.length)]
+    }
+}
+
+.\foreach2.ps1
