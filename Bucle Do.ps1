5028f1fd34b324eb3c58b683bdc4028fe8ad6fdb
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:42:07 -0700

    Create Bucle Do.ps1

---
+++ Bucle Do.ps1
@@ -0,0 +1,27 @@
+﻿cat .\do-while.ps1
+$valor = 5
+$multiplicacion = 1
+do
+{
+    $multiplicacion = $multiplicacion * $valor
+    $valor--
+}
+while ($valor -gt 0)
+
+Write-Host $multiplicacion
+
+.\do-while.ps1
+
+cat .\do-until.ps1
+$valor = 5
+$multiplicacion = 1
+do
+{
+    $multiplicacion = $multiplicacion * $valor
+    $valor--
+}
+until ($valor -eq 0)
+
+Write-Host $multiplicacion
+
+.\do-until.ps1
