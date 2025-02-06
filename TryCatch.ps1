e1493f2a73d2ef101dd65f248d548861edf69db7
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:43:37 -0700

    Create TryCatch.ps1

---
+++ TryCatch.ps1
@@ -0,0 +1,32 @@
+﻿cat .\try-catch.ps1
+try
+{
+    Write-Output "Todo bien"
+}
+catch
+{
+    Write-Output "Algo lanzo una excepcion"
+    Write-Output $_
+}
+
+try
+{
+    Start-Something -ErrorAction Stop
+}
+catch
+{
+    Write-Output "Algo genero una excepcion o uso Write-Error"
+    Write-Output $_
+}
+
+$comando = [System.Data.SqlClient.SqlCommand]::New(queryString, connection)
+try
+{
+    $comando.Connection.Open()
+    $comando.ExecuteNonQuery()
+}
+finally
+{
+    Write-Error "Ha habido un problema con la ejecución de la query. Cerrando la conexión"
+    $comando.Connection.Close()
+}
