23b5477cca5e14d498a65c8e0db18a68d609ac45
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:42:27 -0700

    Create Condicionales2.ps1

---
+++ Condicionales2.ps1
@@ -0,0 +1,11 @@
+﻿$numero = 2
+
+if ( $numero -ge 3 ) {
+    Write-Output "El numero [$numero] es mayor o igual que 3"
+}
+elseif ( $numero -lt 2 ) {
+    Write-Output "El numero [$numero] es menor que 2"
+}
+else {
+    Write-Output "El numero [$numero] es igual a 2"
+}
