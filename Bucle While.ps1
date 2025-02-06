2393aabbc06f041cf9690586a85d85f09dc2299f
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:42:13 -0700

    Create Bucle While.ps1

---
+++ Bucle While.ps1
@@ -0,0 +1,18 @@
+﻿cat .\while.ps1
+$num = 0
+
+while ($num -ne 3)
+{
+    $num++
+    Write-Host $num
+}
+
+cat .\while2.ps1
+$num = 0
+
+while ($num -ne 5)
+{
+    if ($num -eq 1) { $num = $num + 3 ; Continue }
+    $num++
+    Write-Host $num
+}
