f32154bf0d1ff2b1b786006a4e53199ef5f75630
RuizBorquezDallana <borquezdallana@gmail.com>
2025-02-06 14:42:57 -0700

    Create Funciones.ps1

---
+++ Funciones.ps1
@@ -0,0 +1,27 @@
+﻿Get-Process -Name Acrobat | Stop-Process
+
+Get-Help -Full Get-Process
+Get-Help -Full Stop-Process
+
+Get-Process
+Get-Process -Name Acrobat | Stop-Process
+Get-Process
+
+Get-Help -Full Get-ChildItem
+Get-Help -Full Get-Clipboard
+
+Get-ChildItem *.txt | Get-Clipboard
+
+Get-Help -Full Stop-Service
+
+Get-Service
+Get-Service Spooler | Stop-Service
+Get-Service
+"Spooler" | Stop-Service
+Get-Service
+
+$miObjeto = [PSCustomObject] @{
+    Name = "Spooler"
+}
+$miObjeto | Stop-Service
+Get-Service
